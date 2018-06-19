7518d0fe16b31d67380e117ddd88820461351701
opmasan <opmasan@gmail.com>
2018-06-19 17:21:30 +0300

    forms

--- src/assets/main.js
+++ src/assets/main.js
@@ -14,7 +14,7 @@ $('#fullpage').fullpage({
     lockAnchors: true,
     anchors: ['1', '2', '3', '4', 'last'],
     navigation: true,
-    navigationTooltips: ['MasterDynamix', 'Success Key', 'We Can', 'Our Expertise', 'Reach Us'],
+    navigationTooltips: ['MasterDynamix', 'Your success', 'What we do', 'Our expertise', 'Reach us'],
     showActiveTooltip: false,
     menu: '#menu',
     sectionsColor: ['#fff',  '#fff', '#fff', '#fff', '#fff'],
@@ -40,8 +40,10 @@ $('#fullpage').fullpage({
         //using anchorLink
         if(anchorLink == 'last'){
             $('*[data-open="popup-trigger"]').addClass('not-active')
+
         } else {
             $('*[data-open="popup-trigger"]').removeClass('not-active')
+
         }
     },
     onLeave: function(index, nextIndex, direction) {
@@ -50,7 +52,7 @@ $('#fullpage').fullpage({
         // first animation
         if( index == 1 && nextIndex == 2 ) {
             $isAnimatedSecond.addClass('animated');
-            $isAnimatedSecond.eq(0).addClass('fadeInLeftBig').css('animation-delay', '.1s');
+            $isAnimatedSecond.eq(0).addClass('fadeInLeftBig').css('animation-delay', '.2s');
             $isAnimatedSecond.eq(1).addClass('fadeInUpBig').css('animation-delay', '.6s');
             // $isAnimatedSecondSingle.addClass('animated rollIn').css('animation-delay', '1.7s');
         }
@@ -58,7 +60,8 @@ $('#fullpage').fullpage({
 
         // second animation
         else if( ( index == 1 || index == 2 ) && nextIndex == 3 ) {
-            $isAnimatedThird.addClass('animated fadeIn').css('animation-delay', '0.7s');
+            $isAnimatedThird.eq(0).addClass('animated fadeIn').css('animation-delay', '0.2s');
+            $isAnimatedThird.eq(1).addClass('animated fadeIn').css('animation-delay', '0.7s');
         }
 
 
@@ -75,8 +78,12 @@ $('#fullpage').fullpage({
         }
     }
 });
+
 $('#arrowAnim').click(function(){
     $.fn.fullpage.moveSectionDown();
+});
+$(document).ready(function() {
+    $('body').fadeTo( "fast", 1 )
 });
 $('#fullpage-partners').fullpage({
     verticalCentered: false,
@@ -88,7 +95,6 @@ $('#fullpage-partners').fullpage({
     menu: '#menu',
     sectionsColor: ['#fff',  '#f7f7f7', '#fff'],
 });
-console.log('main');
 
 
 // Barba.Dispatcher.on('newPageReady', function(current, prev, container) {
@@ -166,3 +172,200 @@ console.log('main');
 // });
 //
 //
+
+function validEmail(email) { // see:
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(email);
+}
+
+function validateHuman(honeypot) {
+    if (honeypot) {  //if hidden form filled up
+        console.log("Robot Detected!");
+        return true;
+    } else {
+        console.log("Welcome Human!");
+    }
+}
+
+// get all data in form and return object
+function getFormData() {
+    var form = document.getElementById("gform");
+    var elements = form.elements; // all form elements
+    var fields = Object.keys(elements).filter(function(k) {
+        // the filtering logic is simple, only keep fields that are not the honeypot
+        return (elements[k].name !== "honeypot");
+    }).map(function(k) {
+        if(elements[k].name !== undefined) {
+            return elements[k].name;
+            // special case for Edge's html collection
+        }else if(elements[k].length > 0){
+            return elements[k].item(0).name;
+        }
+    }).filter(function(item, pos, self) {
+        return self.indexOf(item) == pos && item;
+    });
+    var data = {};
+    fields.forEach(function(k){
+        data[k] = elements[k].value;
+        var str = ""; // declare empty string outside of loop to allow
+                      // it to be appended to for each item in the loop
+        if(elements[k].type === "checkbox"){ // special case for Edge's html collection
+            str = str + elements[k].checked + ", "; // take the string and append
+                                                    // the current checked value to
+                                                    // the end of it, along with
+                                                    // a comma and a space
+            data[k] = str.slice(0, -2); // remove the last comma and space
+                                        // from the  string to make the output
+                                        // prettier in the spreadsheet
+        }else if(elements[k].length){
+            for(var i = 0; i < elements[k].length; i++){
+                if(elements[k].item(i).checked){
+                    str = str + elements[k].item(i).value + ", "; // same as above
+                    data[k] = str.slice(0, -2);
+                }
+            }
+        }
+    });
+
+    // add form-specific values into the data
+    data.formDataNameOrder = JSON.stringify(fields);
+    data.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
+    data.formGoogleSendEmail = form.dataset.email || ""; // no email by default
+
+    console.log(data);
+    return data;
+}
+
+function handleFormSubmit(event) {  // handles form submit withtout any jquery
+    event.preventDefault();           // we are submitting via xhr below
+    var data = getFormData();         // get the values submitted in the form
+
+    /* OPTION: Remove this comment to enable SPAM prevention, see README.md
+     if (validateHuman(data.honeypot)) {  //if form is filled, form will not be submitted
+     return false;
+     }
+     */
+
+    if( data.email && !validEmail(data.email) ) {   // if email is not valid show error
+        var invalidEmail = document.getElementById("email-invalid");
+        if (invalidEmail) {
+            invalidEmail.style.display = "block";
+            return false;
+        }
+    } else {
+        var url = event.target.action;  //
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', url);
+        // xhr.withCredentials = true;
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function() {
+            console.log( xhr.status, xhr.statusText )
+            console.log(xhr.responseText);
+            document.getElementById("gform").style.display = "none"; // hide form
+            var thankYouMessage = document.getElementById("thankyou_message");
+            if (thankYouMessage) {
+                thankYouMessage.style.display = "block";
+            }
+            return;
+        };
+        // url encode form data for sending as post data
+        var encoded = Object.keys(data).map(function(k) {
+            return encodeURIComponent(k) + "=" + encodeURIComponent(data[k])
+        }).join('&')
+        xhr.send(encoded);
+    }
+}
+function loaded() {
+    console.log("Contact form submission handler loaded successfully.");
+    // bind to the submit event of our form
+    var form = document.getElementById("gform");
+    form.addEventListener("submit", handleFormSubmit, false);
+};
+document.addEventListener("DOMContentLoaded", loaded, false);
+
+// get all data in form and return object
+function getFormData2() {
+    var form = document.getElementById("gform2");
+    var elements = form.elements; // all form elements
+    var fields = Object.keys(elements).map(function(k) {
+        if(elements[k].name !== undefined) {
+            return elements[k].name;
+            // special case for Edge's html collection
+        }else if(elements[k].length > 0){
+            return elements[k].item(0).name;
+        }
+    }).filter(function(item, pos, self) {
+        return self.indexOf(item) == pos && item;
+    });
+    var data = {};
+    fields.forEach(function(k){
+        data[k] = elements[k].value;
+        var str = ""; // declare empty string outside of loop to allow
+                      // it to be appended to for each item in the loop
+        if(elements[k].type === "checkbox"){ // special case for Edge's html collection
+            str = str + elements[k].checked + ", "; // take the string and append
+                                                    // the current checked value to
+                                                    // the end of it, along with
+                                                    // a comma and a space
+            data[k] = str.slice(0, -2); // remove the last comma and space
+                                        // from the  string to make the output
+                                        // prettier in the spreadsheet
+        }else if(elements[k].length){
+            for(var i = 0; i < elements[k].length; i++){
+                if(elements[k].item(i).checked){
+                    str = str + elements[k].item(i).value + ", "; // same as above
+                    data[k] = str.slice(0, -2);
+                }
+            }
+        }
+    });
+
+    // add form-specific values into the data
+    data.formDataNameOrder = JSON.stringify(fields);
+    data.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
+    data.formGoogleSendEmail = form.dataset.email || ""; // no email by default
+
+    console.log(data);
+    return data;
+}
+
+function handleFormSubmit2(event) {  // handles form submit withtout any jquery
+    event.preventDefault();           // we are submitting via xhr below
+    var data = getFormData2();         // get the values submitted in the form
+
+    /* OPTION: Remove this comment to enable SPAM prevention, see README.md
+     if (validateHuman(data.honeypot)) {  //if form is filled, form will not be submitted
+     return false;
+     }
+     */
+
+    if( !validEmail(data.email) ) {   // if email is not valid show error
+        document.getElementById('email-invalid2').style.display = 'block';
+        return false;
+    } else {
+        var url = event.target.action;  //
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', url);
+        // xhr.withCredentials = true;
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onreadystatechange = function() {
+            console.log( xhr.status, xhr.statusText )
+            console.log(xhr.responseText);
+            document.getElementById('gform2').style.display = 'none'; // hide form
+            document.getElementById('thankyou_message2').style.display = 'block';
+            return;
+        };
+        // url encode form data for sending as post data
+        var encoded = Object.keys(data).map(function(k) {
+            return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+        }).join('&')
+        xhr.send(encoded);
+    }
+}
+function loaded2() {
+    console.log('contact form submission handler loaded successfully');
+    // bind to the submit event of our form
+    var form = document.getElementById('gform2');
+    form.addEventListener("submit", handleFormSubmit2, false);
+};
+document.addEventListener('DOMContentLoaded', loaded2, false);
